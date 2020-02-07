ced38badc7d50646a374f08a4b0899065dc2e362
abeneyto <alvarobeneytohurtado@gmail.com>
2020-02-07 21:04:11 +0100

    Create dataclass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularMaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
 import { ListUserComponent } from './components/list-user/list-user.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ListUserComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data/user.ts
@@ -0,0 +1,7 @@
+import DateTimeFormat = Intl.DateTimeFormat;
+
+export class Student {
+  private id;
+  private name;
+  private birthdate: Date;
+}
