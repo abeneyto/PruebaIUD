79663f3cafe306c677b0a2c5d9a9d854ba682077
abeneyto <alvarobeneytohurtado@gmail.com>
2020-02-07 20:40:54 +0100

    Create add, list and edit components. Added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list-user' },
+  { path: 'add-user', component: AddUserComponent },
+  { path: 'edit-user/:id', component: EditUserComponent },
+  { path: 'list-user', component: ListUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
